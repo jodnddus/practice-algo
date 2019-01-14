81819fe4cb03a6c2cfe64c601a6665884ab63ff4
jodnddus <lockhost23@gmail.com>
2019-01-15 00:16:54 +0900

    인접 리스트 구현완료

--- graph/Graph.cc
+++ graph/Graph.cc
@@ -7,27 +7,29 @@ using namespace std;
 //무방향 무가중치 그래프
 int main()
 {
-	vector<int> graphList[100];
+	vector<int> Graph[100];
+
 	int node,edge;
-	cin >> node  >> edge;
+	cout << "노드와 엣지의 갯수 입력: ";
+	cin >> node >> edge;
 
-	for(int i=0;i<node;i++)
+	cout << "인접리스트 입력 ㄱㄱ" << endl;;
+	for(int i=0;i<edge;i++)
 	{
-		int node1, node2;
+		int node1,node2;
 		cin >> node1 >> node2;
-		graphList[node1].push_back(node2);
-		graphList[node2].push_back(node1);
-	}
+		Graph[node1].push_back(node2);
+		Graph[node2].push_back(node1);
+	}	
 
-	cout << "인접리스트 출력" << endl;
-	for(int i=1;i<=node;i++)
+	for(int i=0;i<=edge;i++)
 	{
-		cout << "node" << i << " :";
-		for(int j=0;j<graphList[i].size();j++)
+		cout << "node" << i << ": ";
+		for(int j=0;j<Graph[i].size();j++)
 		{
-			cout << graphList[i][j];
+			cout << Graph[i][j] << " ";
 		}
-		cout << "\n";
+		cout << endl;
 	}
 	return 0;
 }
